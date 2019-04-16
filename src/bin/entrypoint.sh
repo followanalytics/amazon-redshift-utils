b83ee7e6ae5f004e9b97fd6007b78ffb00acde6b
Thomas Lecavelier <ook@ookook.fr>
2019-04-16 10:45:40 +0200

    followanalytics/fa-issues#5595 declare fa-analyze-vacuum, which wrap analyze-vacuum with a guard

--- src/bin/entrypoint.sh
+++ src/bin/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 
 PROJECT=${1:-}
 
-if [ "${PROJECT}" == "analyze-vacuum" ]; then bin/run-analyze-vacuum-utility.sh
+if [ "${PROJECT}" == "fa-analyze-vacuum" ]; then bin/run-fa-vacuum-guard.sh
+elif [ "${PROJECT}" == "analyze-vacuum" ]; then bin/run-analyze-vacuum-utility.sh
 elif [ "${PROJECT}" == "column-encoding" ]; then bin/run-column-encoding-utility.sh
 elif [ "${PROJECT}" == "unload-copy" ]; then bin/run-unload-copy-utility.sh
 elif [ "${PROJECT}" == "user-last-login" ]; then bin/run-user-last-login.sh
-else echo "Unhandled arg for project to run. Please select from either 'analyze-vacuum', 'column-encoding' or 'unload-copy'"
+else echo "Unhandled arg for project to run. Please select from either 'fa-analyze-vacuum', 'analyze-vacuum', 'column-encoding' or 'unload-copy'"
 fi
